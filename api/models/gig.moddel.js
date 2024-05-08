1742b75adb6c8b579dd6127a68d4b493d54ab07c
suryansh-f-r-i-d-a-y <suryansh2465.be20@chitkara.edu.in>
2024-05-08 21:17:41 +0530

    models

---
+++ api/models/gig.moddel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GigSchema = new Schema(
+    {
+        userId:{
+            type: String,
+            required: true,
+        },
+        title:{
+            type: String,
+            required: true,
+        },
+        desc:{
+            type: String,
+            required: true,
+        },
+        totalStars:{
+            type: Number,
+            default: 0,
+        },
+        starNumber:{
+            type: Number,
+            required: true,
+        },
+        cat:{
+            type: String,
+            required: true,
+        },
+        price:{
+            type: Number,
+            required: true,
+        },
+        cover:{
+            type: String,
+            required: true,
+        },
+        images:{
+            type: [String],
+            required: false,
+        },
+        shortTitle:{
+            type: String,
+            required: true,
+        },
+        shortDesc:{
+            type: String,
+            required: true,
+        },
+        deliveryTime:{
+            type: Number,
+            required: true,
+        },
+        revisionNumber:{
+            type: Number,
+            required: true,
+        },
+        features:{
+            type: [String],
+            required: false,
+        },
+        sales:{
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('Gig', GigSchema)
