ea4a309ff0548411307808deab92e7981cd89de6
suryansh-f-r-i-d-a-y <78611264+suryansh-f-r-i-d-a-y@users.noreply.github.com>
2024-05-08 18:54:08 +0000

    user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,37 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+        country: {
+            type: String,
+            required: false,
+        },
+        phone: {
+            type: String,
+            required: false,
+        },
+        desc: {
+            type: String,
+            required: false,
+        },
+        isSeller: {
+            type: Boolean,
+            default: false,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
+    {
+        timestamps: true,
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        required: false,
-    },
-    country: {
-        type: String,
-        required: false,
-    },
-    phone: {
-        type: String,
-        required: false,
-    }
+);
 
-
-
-
-
-})
+export default mongoose.model('User' , userSchema)
